df44846b0149e1482974eef1858423740579289c
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-12 22:11:14 -0500

    05 examples

---
+++ 05-builtinobjects/main.js
@@ -0,0 +1,21 @@
+
+console.log(Date.now());
+
+console.log(Math.floor(1.3));
+
+let number = 5;
+
+number.toString()
+
+console.log(typeof(number))
+
+let user = {
+    nombre: 'Arturo',
+    apellido: 'Ortueta'
+}
+
+console.log(JSON.stringify(user))
+
+let manuel = JSON.stringify(user)
+
+console.log(JSON.parse(manuel))
