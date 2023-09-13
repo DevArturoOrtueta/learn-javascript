7546cf8249dbdca793ac51491a428639f062530d
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-12 20:54:37 -0500

    Update

--- 02-hoisting/main.js
+++ 02-hoisting/main.js
@@ -1,13 +1,14 @@
 
 MiNombre()
+let age = 27;
 
 function MiNombre() {
     let nombre = 'Arturo Ortueta';
     console.log(nombre);
+    //console.log(age); //En este ejemplo lo que ocurre es que busca el scope mas alto en el que lo utilizan, las variables en su lugar si deben ser declaradas antes de su ejecucion, o almenos inicializadas.
+    let age = 27;
+    console.log(age);
 }
 
-console.log(age);
-
-let age = 27;
 
 //El Hoisting nos permite utilizar variables y funciones antes de ser declaradas, esto debido a que Javascript las almacena en memoria en la fase de compilacion.
