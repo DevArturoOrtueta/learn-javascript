b6afca7f74c286292ee516907506b66f5cb89842
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-12 20:50:36 -0500

    Update hoisting

--- 02-hoisting/main.js
+++ 02-hoisting/main.js
@@ -3,5 +3,11 @@ MiNombre()
 
 function MiNombre() {
     let nombre = 'Arturo Ortueta';
-    console.log(nombre)
+    console.log(nombre);
 }
+
+console.log(age);
+
+let age = 27;
+
+//El Hoisting nos permite utilizar variables y funciones antes de ser declaradas, esto debido a que Javascript las almacena en memoria en la fase de compilacion.
