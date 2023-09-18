7acb62138fa9809e6aa014b77b419a5aa8bdd09b
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-18 16:11:29 -0500

    Map

---
+++ 09-keyed-collections/main.js
@@ -0,0 +1,16 @@
+// Script de colecciones
+
+// Map
+
+const map1 = new Map();
+
+map1.set('nombre', 'Arturo');
+map1.set('apellido', 'Ortueta')
+
+console.log(map1.entries());
+
+console.log(map1.keys())
+
+console.log(map1.values())
+
+console.log(map1.get(nombre));
