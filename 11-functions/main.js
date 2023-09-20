a699e5777abd08b90a6b6e57f2f34c78a1009066
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-20 13:47:37 -0500

    Predefined functions

--- 11-functions/main.js
+++ 11-functions/main.js
@@ -29,3 +29,39 @@ Hois();
 function Hois(){
     console.log('Funciona el hoisting')
 }
+
+// Predefined functions
+// JavaScript has several top-level, built-in functions:
+
+// eval()
+// The eval() method evaluates JavaScript code represented as a string.
+
+// isFinite()
+// The global isFinite() function determines whether the passed value is a finite number. If needed, the parameter is first converted to a number.
+
+// isNaN()
+// The isNaN() function determines whether a value is NaN or not. Note: coercion inside the isNaN function has interesting rules; you may alternatively want to use Number.isNaN() to determine if the value is Not-A-Number.
+
+// parseFloat()
+// The parseFloat() function parses a string argument and returns a floating point number.
+
+// parseInt()
+// The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems).
+
+// decodeURI()
+// The decodeURI() function decodes a Uniform Resource Identifier (URI) previously created by encodeURI or by a similar routine.
+
+// decodeURIComponent()
+// The decodeURIComponent() method decodes a Uniform Resource Identifier (URI) component previously created by encodeURIComponent or by a similar routine.
+
+// encodeURI()
+// The encodeURI() method encodes a Uniform Resource Identifier (URI) by replacing each instance of certain characters by one, two, three, or four escape sequences representing the UTF-8 encoding of the character (will only be four escape sequences for characters composed of two "surrogate" characters).
+
+// encodeURIComponent()
+// The encodeURIComponent() method encodes a Uniform Resource Identifier (URI) component by replacing each instance of certain characters by one, two, three, or four escape sequences representing the UTF-8 encoding of the character (will only be four escape sequences for characters composed of two "surrogate" characters).
+
+// escape() Deprecated
+// The escape() method computes a new string in which certain characters have been replaced by a hexadecimal escape sequence. It's deprecated and you should use encodeURI() or encodeURIComponent() instead.
+
+// unescape() Deprecated
+// The unescape() method computes a new string in which hexadecimal escape sequences are replaced with the character that it represents. The escape sequences might be introduced by a function like escape(). It's deprecated and you should use decodeURI() or decodeURIComponent() instead.
