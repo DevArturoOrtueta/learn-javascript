e0b2673d068df2f3c82e80b9e1ddf2dbb5557af0
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-20 10:26:11 -0500

    Declaracion de funciones

---
+++ 11-functions/main.js
@@ -0,0 +1,9 @@
+// Declaracion de funciones
+
+function Funcion1 () {
+    console.log('Declarado con la alabra reservada function')
+}
+
+const Function2 = () => {
+    console.log('Declarado como const')
+}
