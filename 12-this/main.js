4453b62a27d87babb497bca672e2a8bea527ab2c
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-23 09:46:35 -0500

    Update

--- 12-this/main.js
+++ 12-this/main.js
@@ -75,3 +75,26 @@ const obj  = {
     fo2.call(this) // Use this instead of fo2()
   },
 };
+
+// function borrowing
+
+// https://medium.com/@ensallee/function-borrowing-in-javascript-4bd671e9d7b4
+
+class Dog {
+    constructor(name, age, breed) {
+      this.name = name
+      this.age = age
+      this.breed = breed
+    }
+    tellUsAboutYourSelf() {
+      return `My name is ${this.name}. I am a ${this.breed} and I am ${this.age} years old.`
+    }
+  
+    woof() {
+      return "WOOF!!!"
+    }
+  }
+  
+  let fido = new Dog("Fido", 3, "dachshund")
+  fido.tellUsAboutYourSelf()
+  //=> 'My name is Fido. I am a dachshund and I am 3 years old.'
