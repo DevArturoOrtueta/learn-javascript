e2a8687d3797f6f0c81faf4ca2377f7db0e89e09
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-23 08:35:13 -0500

    Update unbound this

--- 12-this/main.js
+++ 12-this/main.js
@@ -30,3 +30,24 @@ let users = {
   users = null; // overwrite to make things obvious
   
   admin.sayHi(); // TypeError: Cannot read property 'name' of null
+
+//   Unbound this
+
+let userr = { name: "John" };
+let adminn = { name: "Admin" };
+
+function sayHi() {
+  alert( this.name );
+}
+
+// use the same function in two objects
+userr.f = sayHi;
+adminn.f = sayHi;
+
+// these calls have different this
+// "this" inside the function is the object "before the dot"
+userr.f(); // John  (this == user)
+adminn.f(); // Admin  (this == admin)
+
+adminn['f'](); // Admin (dot or square brackets access the method – doesn't matter)
+
