210b6ebafec942a644c4de534f0abbbb6c3efc82
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-09-23 09:03:08 -0500

    arrow function and event handler

--- 12-this/main.js
+++ 12-this/main.js
@@ -51,3 +51,27 @@ adminn.f(); // Admin  (this == admin)
 
 adminn['f'](); // Admin (dot or square brackets access the method – doesn't matter)
 
+// This en arrow function
+
+// Arrow functions have no this value in their scope, so you can access this value of the object. But Normal functions have this value in their scope, in this example this value of the normal function is globalThis or window. and it allows to you access the global scope. if you call fo2 with this value of object instead of globalThis you get '"hey"'
+
+var greeting = 'hi';
+
+const obj  = {
+  greeting: 'hey',
+
+  fo() {
+    const greeting = 'hola';
+
+    const fo2 = function () {
+      const greeting = 'hello';
+
+      const arrowFo = () => {
+        console.log(this.greeting);
+      };
+
+      arrowFo();
+    };
+    fo2.call(this) // Use this instead of fo2()
+  },
+};
