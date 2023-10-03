419d35e5ed4a3347637eee37a275f6a034e79cbc
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-10-02 21:02:10 -0500

    Classes

--- 13-asynchronous/main.js
+++ 13-asynchronous/main.js
@@ -1,26 +1,3 @@
-const log = document.querySelector(".event-log");
-
-document.querySelector("#xhr").addEventListener("click", () => {
-  log.textContent = "";
-
-  const xhr = new XMLHttpRequest();
-
-  xhr.addEventListener("loadend", () => {
-    log.textContent = `${log.textContent}Finished with status: ${xhr.status}`;
-  });
-
-  xhr.open(
-    "GET",
-    "https://raw.githubusercontent.com/mdn/content/main/files/en-us/_wikihistory.json",
-  );
-  xhr.send();
-  log.textContent = `${log.textContent}Started XHR request\n`;
-});
-
-document.querySelector("#reload").addEventListener("click", () => {
-  log.textContent = "";
-  document.location.reload();
-});
 
 // Callbacks
 
@@ -37,3 +14,52 @@ const names = ['Pedro', 'Manuel', 'Juan']
 modify(names, function (array) {
     console.log(`He modificado el array y ahora es de ${array.length} elementos!`)
 })
+
+// Promises
+
+const dato = [{
+    id: 1,
+    title: 'Iron Man',
+    year: 2008
+},
+{
+    id: 2,
+    title: 'Spiderman: Homecoming',
+    year: 2017
+},
+{
+    id: 3,
+    title: 'Avengers: Endgame',
+    year: 2019
+}];
+
+// const getDatos = () => {
+//     return datos;
+// }
+
+
+const getDatos = () => {
+    return new Promise((resolve, reject) => {
+        if(dato.length === 0){
+            reject(new Error('No existen datos'))
+        }
+        setTimeout(() => {
+           resolve(dato);
+        }, 1500)
+    })
+    
+}
+
+// console.log(getDatos()
+//     .then((datos) => console.log(datos))); 
+const fetchingData = async () => {
+    try {
+        const datos = await getDatos();
+        console.log(datos);
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+fetchingData()
---
+++ 14-classes/main.js
@@ -0,0 +1,133 @@
+// Classes
+
+// class Rectangulo {
+//     constructor(alto, ancho){
+//         this.alto = alto;
+//         this.ancho = ancho;
+//     }
+// }
+
+// console.log(Rectangulo.name);
+// output: "Rectangulo"
+
+// Nombrada
+// let Rectangulo = class Rectangulo2 {
+//   constructor(alto, ancho) {
+//     this.alto = alto;
+//     this.ancho = ancho;
+//   }
+// };
+// console.log(Rectangulo.name);
+// output: "Rectangulo2"
+
+class Rectangulo {
+    constructor(alto, ancho) {
+      this.alto = alto;
+      this.ancho = ancho;
+    }
+    // Getter
+    get area() {
+      return this.calcArea();
+    }
+    // Método
+    calcArea() {
+      return this.alto * this.ancho;
+    }
+  }
+  
+  const cuadrado = new Rectangulo(10, 10);
+  
+  console.log(cuadrado.area); // 100
+
+//   Metodos estaticos
+
+class Punto {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    static distancia(a, b) {
+      const dx = a.x - b.x;
+      const dy = a.y - b.y;
+  
+      return Math.sqrt(dx * dx + dy * dy);
+    }
+  }
+  
+  const p1 = new Punto(5, 5);
+  const p2 = new Punto(10, 10);
+  
+  console.log(Punto.distancia(p1, p2)); // 7.0710678118654755
+
+//   Boxing con prototipos y metodos estaticos
+
+class Animal {
+    hablar() {
+      return this;
+    }
+    static comer() {
+      return this;
+    }
+  }
+  
+  let obj = new Animal();
+  obj.hablar(); // Animal {}
+  let hablar = obj.hablar;
+  hablar(); // undefined
+  
+  Animal.comer() // class Animal
+  let comer = Animal.comer;
+  comer(); // undefined
+
+//   Subclases con extends
+
+class Animal {
+    constructor(nombre) {
+      this.nombre = nombre;
+    }
+  
+    hablar() {
+      console.log(this.nombre + " hace un ruido.");
+    }
+  }
+  
+  class Perro extends Animal {
+    hablar() {
+      console.log(this.nombre + " ladra.");
+    }
+  }
+
+//   Especies
+
+class MyArray extends Array {
+    // Sobreescribe species sobre el constructor padre Array
+    static get [Symbol.species]() {
+      return Array;
+    }
+  }
+  
+  var a = new MyArray(1, 2, 3);
+  var mapped = a.map((x) => x * x);
+  
+  console.log(mapped instanceof MyArray); // false
+  console.log(mapped instanceof Array); // true
+
+//   Llamadas a superclases con super
+
+class Gato {
+    constructor(nombre) {
+      this.nombre = nombre;
+    }
+  
+    hablar() {
+      console.log(this.nombre + ' hace ruido.');
+    }
+  }
+  
+  class Leon extends Gato {
+    hablar() {
+      super.hablar();
+      console.log(this.nombre + ' maulla.');
+    }
+  }
